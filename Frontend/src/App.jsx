176a93028de298c2fc4e4e1b0119be45ab8d8ee3
Sourav <garaisourav143@gmail.com>
2024-05-26 19:35:04 +0530

    footer ready

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Footer></Footer>
     </>
   );
 }
