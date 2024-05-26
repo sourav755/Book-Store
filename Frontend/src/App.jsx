33beb8789c56073fc94ea83a180d21f83d1d0eef
Sourav <garaisourav143@gmail.com>
2024-05-26 17:53:30 +0530

    banner ready

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Banner></Banner>
     </>
   );
 }
