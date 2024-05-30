3a5d015706134011917840c3c52895bf0c24e153
Sourav <garaisourav143@gmail.com>
2024-05-30 23:58:50 +0530

    Api integration from backend using axios

--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import list from "../../public/list.json";
 import Cards from "./Cards";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+      } catch (error) {}
+    };
+  }, []);
   return (
     <>
       <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
