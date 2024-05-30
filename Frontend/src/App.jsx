a4d8ab73499a24a70ac6cb44fb2111227871fda7
Sourav <garaisourav143@gmail.com>
2024-05-30 13:14:19 +0530

    sign up btn created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Courses from "./courses/Courses";
 import Home from "./home/Home";
+import Signup from "./components/Signup";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/course" element={<Courses />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
         </Routes>
       </div>
     </>
--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
@@ -7,9 +8,11 @@ function Login() {
         <div className="modal-box">
           <form method="dialog">
             {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-              ✕
-            </button>
+            <Link to={"/"}>
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                ✕
+              </button>
+            </Link>
           </form>
           <h3 className="font-bold text-lg">Login</h3>
           {/* Email */}
@@ -31,6 +34,20 @@ function Login() {
               placeholder="Enter your email"
               className="w-80 px-3 py-1 border rounded-md outline-none"
             />
+          </div>
+          <div className="flex justify-around mt-4">
+            <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200">
+              Login
+            </button>
+            <p>
+              Not Registered?
+              <Link
+                to="/signup"
+                className="underline text-blue-500 cursor-pointer"
+              >
+                Signup
+              </Link>
+            </p>
           </div>
         </div>
       </dialog>
